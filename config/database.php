1ace36d2ddee633391050e16a69b4a289f6ce48a
shellking <donaldkant2000@gmail.com>
2021-03-02 20:14:31 +0100

    set up database config on local branch

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
 use Illuminate\Support\Str;
-$url = parse_url(getenv("DATABASE_URL"));
+/*$url = parse_url(getenv("DATABASE_URL"));
 $host = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
-$database = substr($url["path"], 1);
+$database = substr($url["path"], 1);*/
 
 return [
 
@@ -69,10 +69,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => $host,
-            'database' => $database,
-            'username' => $username,
-            'password' => $password,
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
